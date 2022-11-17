Fixed covariance calculation

PCA/src/criteria.py
@@ -58,11 +58,12 @@ def median_criteria(eigvals: np.ndarray):
     a threshold for the signal related eigenvalues
     """
     median = np.median(eigvals)
+    eigvals *= (1 / median)
     eigvals_t = []
     # first condition
     for i in range(len(eigvals)):
-        if np.sqrt(eigvals[i]) < 2 * np.sqrt(median):
-            eigvals_t.append(eigvals[i])
+        if np.sqrt(eigvals[i]) < 2:
+            eigvals_t.append(eigvals[i] * abs(median))
     eigvals_t = np.array(eigvals_t)
 
     # second conditions
@@ -79,8 +80,6 @@ def median_criteria(eigvals: np.ndarray):
         # k_med = PC_list.index(max(PC_list))
         k_med = np.where(eigvals == max(PC_list))[0]
     else:
-        k_med = [eigvals.shape[0] -1 ]
+        k_med = [eigvals.shape[0] - 1]
 
     return k_med[0]
-
-

PCA/src/denoise.py
@@ -3,7 +3,7 @@ from .utils import *
 from .criteria import *
 
 
-def pca(img: np.ndarray, mask: np.ndarray, criteria: str) -> np.ndarray:
+def pca(img: np.ndarray, criteria: str, mask: np.ndarray | None = None) -> np.ndarray:
     """
     :param img: noisy 2D CEST image (x,y,ndyn)
     :param mask: 2D binary mask (x,y)
@@ -30,7 +30,7 @@ def step1(img: np.ndarray, mask: np.ndarray) -> np.ndarray:
 
 def step2(C_tilde):
     """Step 2: Principal component analyses"""
-    cov_C_tilde = np.cov(C_tilde)
+    cov_C_tilde = cov(C_tilde)
     eigvals, eigvecs = calc_eig(cov_C_tilde, 'max')
     return eigvals, eigvecs
 

PCA/src/utils.py
@@ -12,9 +12,19 @@ def img_to_casorati_matrix(img: np.array, mask: np.array = None) -> np.array:
             continue
         casorati_matrix.append(img[i2, i1, :])
     return np.array(casorati_matrix)
+
+
+def cov(C_tilde):
+    # cov(C) = 1/ (n - 1) np.transpose(C_tilde) C_tilde = np.transpose(phi) ∧ phi
+    # where:    n       - number of z-spectra dynamics
+    #           C_tilde - colum-wise mean centered Casorati matrix
+    #           phi     - n x n orthogonal eigenvector matrix
+    #           ∧       - diag(λ_1, λ_2, ....) with λ_i being the eigenvalue i (λ_1 > λ_2 > .....)
+    cov = 1 / (C_tilde.shape[1] - 1) * np.dot(np.transpose(C_tilde), C_tilde)
+    return cov
+
 def calc_eig(matrix: np.array, order: str) -> tuple[np.ndarray, np.ndarray]:
     eigvals, eigvecs = np.linalg.eig(matrix)
-    #if order is not None:
     if order == 'max':
         idx = eigvals.argsort()[::-1]
     if order == 'min':