229a89c9a18b6884bacef27fc4e5a1b9459d6349
ludgerradke <radke@intervall.io>
2022-11-22 08:41:38 +0100

    Test run time of the different methods.

--- test/test_circle.py
+++ test/test_circle.py
@@ -2,8 +2,8 @@ import time
 
 from Loader.loader import LoaderCEST
 from PCA import pca
-from NLM import nlm
-from BM3D import bm3d
+from NLM import nlm, nlm_CEST
+from BM3D import bm3d, bm3d_CEST
 from pathlib import Path
 
 
@@ -13,5 +13,15 @@ if __name__ == '__main__':
 
     Z, mask, file = loader.__getitem__(0)
 
-    _ = bm3d((Z[:64, :64, 0] * 255).astype('int16'))
-    b = 2
+    start_pca = time.time()
+    Z_pca = pca(Z, 'nelson')
+    end_pca = time.time()
+    start_bm3d = end_pca
+    Z_bm3d = bm3d_CEST(Z, None, True)
+    end_bm3d = time.time()
+    start_nlm = end_bm3d
+    Z_nlm = nlm_CEST(Z, 20, 8, False)
+    end_nlm = time.time()
+    print(f"PCA duration: {end_pca - start_pca}")
+    print(f"BM3D duration: {end_bm3d - start_bm3d}")
+    print(f"NLM duration: {end_nlm - start_nlm}")
