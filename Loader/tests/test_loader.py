2b3ccd98212370e3745770ccff2dbab790bf3d6c
ludgerradke <radke@intervall.io>
2022-12-19 15:33:21 +0100

    Loader updated and test added.

--- Loader/tests/test_loader.py
+++ Loader/tests/test_loader.py
@@ -6,60 +6,36 @@ import nibabel as nib
 from Loader.src.loader import *
 
 
-class TestLoadFunctions(unittest.TestCase):
+class TestLoader(unittest.TestCase):
     def setUp(self):
         # Create a temporary directory for the test data
         self.temp_dir = tempfile.TemporaryDirectory()
-        # Create a dummy NIfTI file for testing
-        self.nii_file = Path(self.temp_dir.name) / 'test.nii'
+        # Create a dummy image file for testing
+        self.image_file = Path(self.temp_dir.name) / 'image.nii'
         img = np.zeros((10, 10, 10))
-        nib.save(nib.Nifti1Image(img, np.eye(4)), self.nii_file)
+        nib.save(nib.Nifti1Image(img, np.eye(4)), self.image_file)
+        # Create a dummy mask file for testing
+        self.mask_file = Path(self.temp_dir.name) / 'mask.nii.gz'
+        img = np.ones((10, 10, 10))
+        nib.save(nib.Nifti1Image(img, np.eye(4)), self.mask_file)
+        # Create a dummy LoaderCEST instance for testing
+        self.loader = LoaderCEST(self.image_file)
 
-    def test_load_nii(self):
-        # Test loading data from a NIfTI file
-        data = load_nii(self.nii_file)
-        # Assert that the data is a NumPy array
-        self.assertIsInstance(data, np.ndarray)
+    def test_len(self):
+        # Test the __len__ method of the LoaderCEST class
+        self.assertEqual(len(self.loader), 1)
 
-    def test_load_z_spectra(self):
-        # Test loading Z spectra data from a NIfTI file
-        data = load_z_spectra(self.nii_file)
-        # Assert that the data is a NumPy array
-        self.assertIsInstance(data, np.ndarray)
+    def test_getitem(self):
+        # Test the __getitem__ method of the LoaderCEST class
+        spectra, mask, file = self.loader[0]
+        # Assert that the returned values are NumPy arrays
+        self.assertIsInstance(spectra, np.ndarray)
+        self.assertIsInstance(mask, np.ndarray)
+        # Assert that the returned path is a Path object
+        self.assertIsInstance(file, Path)
+        # Assert that the returned path is the correct path
+        self.assertEqual(file, self.image_file)
 
-
-class TestGetFiles(unittest.TestCase):
-    def setUp(self):
-        # Create a temporary directory for the test data
-        self.temp_dir = tempfile.TemporaryDirectory()
-        # Create a dummy root directory for testing
-        self.root_dir = Path(self.temp_dir.name) / 'root'
-        self.root_dir.mkdir()
-        # Create some dummy files in the root directory
-        for i in range(5):
-            Path(self.root_dir / f'test{i}.txt').touch()
-        for i in range(5):
-            Path(self.root_dir / f'test{i}.nii').touch()
-
-    def test_get_files(self):
-        # Test getting a list of files from the root directory
-        files = get_files(self.root_dir)
-        # Assert that the returned value is a list of Path objects
-        self.assertIsInstance(files, list)
-        self.assertIsInstance(files[0], Path)
-        # Assert that the list has the expected number of files
-        self.assertEqual(len(files), 10)
-
-    def test_get_files_with_pattern(self):
-        # Test getting a list of files from the root directory that match a pattern
-        files = get_files(self.root_dir, pattern='.nii')
-        # Assert that the returned value is a list of Path objects
-        self.assertIsInstance(files, list)
-        self.assertIsInstance(files[0], Path)
-        # Assert that the returned list only contains NIfTI files
-        self.assertTrue(all(str(f).endswith('.nii') for f in files))
-        # Assert that the list has the expected number of files
-        self.assertEqual(len(files), 5)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ Loader/tests/test_utils.py
@@ -0,0 +1,63 @@
+import tempfile
+import unittest
+
+from Loader.src.utils import *
+
+
+class TestLoadFunctions(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory for the test data
+        self.temp_dir = tempfile.TemporaryDirectory()
+        # Create a dummy NIfTI file for testing
+        self.nii_file = Path(self.temp_dir.name) / 'test.nii'
+        img = np.zeros((10, 10, 10))
+        nib.save(nib.Nifti1Image(img, np.eye(4)), self.nii_file)
+
+    def test_load_nii(self):
+        # Test loading data from a NIfTI file
+        data = load_nii(self.nii_file)
+        # Assert that the data is a NumPy array
+        self.assertIsInstance(data, np.ndarray)
+
+    def test_load_z_spectra(self):
+        # Test loading Z spectra data from a NIfTI file
+        data = load_z_spectra(self.nii_file)
+        # Assert that the data is a NumPy array
+        self.assertIsInstance(data, np.ndarray)
+
+
+class TestGetFiles(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory for the test data
+        self.temp_dir = tempfile.TemporaryDirectory()
+        # Create a dummy root directory for testing
+        self.root_dir = Path(self.temp_dir.name) / 'root'
+        self.root_dir.mkdir()
+        # Create some dummy files in the root directory
+        for i in range(5):
+            Path(self.root_dir / f'test{i}.txt').touch()
+        for i in range(5):
+            Path(self.root_dir / f'test{i}.nii').touch()
+
+    def test_get_files(self):
+        # Test getting a list of files from the root directory
+        files = get_files(self.root_dir)
+        # Assert that the returned value is a list of Path objects
+        self.assertIsInstance(files, list)
+        self.assertIsInstance(files[0], Path)
+        # Assert that the list has the expected number of files
+        self.assertEqual(len(files), 10)
+
+    def test_get_files_with_pattern(self):
+        # Test getting a list of files from the root directory that match a pattern
+        files = get_files(self.root_dir, pattern='.nii')
+        # Assert that the returned value is a list of Path objects
+        self.assertIsInstance(files, list)
+        self.assertIsInstance(files[0], Path)
+        # Assert that the returned list only contains NIfTI files
+        self.assertTrue(all(str(f).endswith('.nii') for f in files))
+        # Assert that the list has the expected number of files
+        self.assertEqual(len(files), 5)
+
+if __name__ == '__main__':
+    unittest.main()
