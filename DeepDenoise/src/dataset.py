ea2f18b0042d4025fd198990b989d3ebde8b65f4
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-07-27 10:40:35 +0200

    docstrings added

--- DeepDenoise/src/dataset.py
+++ DeepDenoise/src/dataset.py
@@ -1,5 +1,6 @@
-import os
 from pathlib import Path
+from pathlib import Path
+from typing import List, Dict, Union
 
 import nibabel as nib
 import torch
@@ -9,6 +10,8 @@ from Transform.src import Noiser
 
 
 class CESTDataset(Dataset):
+    """A Dataset class for handling CEST MRI data."""
+
     def __init__(
         self,
         root_dir: Path,
@@ -17,6 +20,25 @@ class CESTDataset(Dataset):
         noise_std: float = 0.1,
         transform=None,
     ):
+        """Init Class.
+
+        Args:
+            root_dir (Path): The root directory that contains the .nii files.
+            mode (str): The mode in which to operate, either "train", "val", or "test".
+            distribution (List[float] | None, optional): The data distribution across training, validation, and testing. Defaults to [0.7, 0.2, 0.1].
+            noise_std (float, optional): The standard deviation of the noise to add. Defaults to 0.1.
+            transform (callable, optional): An optional transformation to apply to the data.
+
+        Raises:
+            ValueError: If an invalid mode is given.
+
+        Attributes:
+            mode (str): The mode in which the dataset operates.
+            root_dir (Path): The root directory containing the .nii files.
+            noiser (Noiser): The Noiser object used to add noise to the images.
+            transform (callable, optional): An optional transformation to apply to the data.
+            file_list (List[Path]): A list of paths to the .nii files.
+        """
 
         # If distribution parameter is not provided, set default values
         distribution = distribution if distribution is not None else [0.7, 0.2, 0.1]
@@ -51,10 +73,23 @@ class CESTDataset(Dataset):
         # Set the file list based on start and end indices
         self.file_list = files[start:end]
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Get the length of the dataset.
+
+        Returns:
+            int: The number of items in the dataset.
+        """
         return len(self.file_list)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: Union[int, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """Get an item from the dataset by index.
+
+        Args:
+            idx (int | torch.Tensor): The index of the item to get.
+
+        Returns:
+            dict[str, torch.Tensor]: The item, consisting of the ground truth and noisy images.
+        """
         # If idx is a tensor, convert it to a Python integer
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -83,7 +118,15 @@ class CESTDataset(Dataset):
         return sample
 
 
-def load_z(img_path):
+def load_z(img_path: Path) -> np.ndarray:
+    """Load a CEST file and normalize.
+
+    Args:
+        img_path (Path): The path to the .nii file.
+
+    Returns:
+        np.ndarray: The normalized image data from the .nii file.
+    """
     img_nii = nib.load(img_path)
     img = img_nii.get_fdata() / 4016
     return img
