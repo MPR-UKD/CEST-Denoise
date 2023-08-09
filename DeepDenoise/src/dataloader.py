8c36bef29861c24e918215889fa614b4d6b8c55b
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-08-09 16:51:46 +0200

    Enhance CESTDataModule and Introduce `dyn` Parameter

--- DeepDenoise/src/dataloader.py
+++ DeepDenoise/src/dataloader.py
@@ -2,46 +2,74 @@ import pytorch_lightning as pl
 from pathlib import Path
 from torch.utils.data import DataLoader
 from DeepDenoise.src.dataset import CESTDataset
-from typing import Callable
+from typing import Callable, Union, List, Optional
 
 
 def get_dataset(
-    dir: str | Path,
+    dir: Union[str, Path],
     mode: str,
-    distribution: list | None,
+    distribution: Optional[List[float]],
     noise_std: float,
-    transform: Callable | None,
-):
+    transform: Optional[Callable],
+    dyn: int
+) -> CESTDataset:
+    """
+    Create and return a CESTDataset.
+
+    Args:
+        dir (Union[str, Path]): Directory containing the .nii files.
+        mode (str): Mode of operation ("train", "val", or "test").
+        distribution (Optional[List[float]]): Data distribution across training, validation, and testing.
+        noise_std (float): Standard deviation of the noise to add.
+        transform (Optional[Callable]): Optional transformation to apply to the data.
+        dyn (int): Number of offset frequencies in the Z-spectrum.
+
+    Returns:
+        CESTDataset: The created dataset.
+    """
     return CESTDataset(
         root_dir=Path(dir),
         mode=mode,
         distribution=distribution,
         noise_std=noise_std,
         transform=transform,
+        dyn=dyn
     )
 
 
 class CESTDataModule(pl.LightningDataModule):
     def __init__(
         self,
-        dir: str | Path,
-        distribution: list | None = None,
+        dir: Union[str, Path],
+        distribution: Optional[List[float]] = None,
         batch_size: int = 1,
         workers: int = 1,
-        transform: None | Callable = None,
+        transform: Optional[Callable] = None,
         noise_std: float = 0.2,
+        dyn: int = 42
     ):
+        """
+        Initialize the CESTDataModule class.
+
+        Args:
+            dir (Union[str, Path]): Directory containing the .nii files.
+            distribution (Optional[List[float]]): Data distribution across training, validation, and testing.
+            batch_size (int): Batch size for data loading.
+            workers (int): Number of workers for data loading.
+            transform (Optional[Callable]): Optional transformation to apply to the data.
+            noise_std (float): Standard deviation of the noise to add.
+            dyn (int): Number of offset frequencies in the Z-spectrum.
+        """
         super().__init__()
         self.dir = dir
-        self.train_dataset = get_dataset(
-            dir, "train", distribution, noise_std, transform
-        )
-        self.val_dataset = get_dataset(dir, "val", distribution, noise_std, transform)
-        self.test_dataset = get_dataset(dir, "test", distribution, noise_std, transform)
+        self.train_dataset = get_dataset(dir, "train", distribution, noise_std, transform, dyn)
+        self.val_dataset = get_dataset(dir, "val", distribution, noise_std, transform, dyn)
+        self.test_dataset = get_dataset(dir, "test", distribution, noise_std, transform, dyn)
         self.batch_size = batch_size
         self.workers = workers
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> DataLoader:
+        """Return a DataLoader for the training dataset."""
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
@@ -49,12 +77,14 @@ class CESTDataModule(pl.LightningDataModule):
             num_workers=self.workers,
         )
 
-    def val_dataloader(self):
+    def val_dataloader(self) -> DataLoader:
+        """Return a DataLoader for the validation dataset."""
         return DataLoader(
             self.val_dataset, batch_size=self.batch_size, num_workers=self.workers
         )
 
-    def test_dataloader(self):
+    def test_dataloader(self) -> DataLoader:
+        """Return a DataLoader for the testing dataset."""
         return DataLoader(
             self.test_dataset, batch_size=self.batch_size, num_workers=self.workers
         )
