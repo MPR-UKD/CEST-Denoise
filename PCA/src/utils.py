283c15d8f16dfdabc583a4627531445cdebcd0ca
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-09-21 13:30:12 +0200

    Docstrings added

--- PCA/src/utils.py
+++ PCA/src/utils.py
@@ -1,56 +1,64 @@
-import numpy as np
 from itertools import product
+from typing import Tuple, Optional
+
+import numpy as np
 
 
-def img_to_casorati_matrix(img: np.array, mask: np.array = None) -> np.array:
-    # Get the shape of the image
+def img_to_casorati_matrix(img: np.ndarray, mask: Optional[np.ndarray] = None) -> np.ndarray:
+    """
+    Converts a given 2D image to a Casorati matrix.
+
+    Args:
+        img (np.ndarray): The 2D image to be converted. Shape (n, m, _).
+        mask (Optional[np.ndarray]): The binary mask applied to the image. Shape (n, m). Defaults to None.
+
+    Returns:
+        np.ndarray: The Casorati matrix obtained from the provided image.
+    """
     n, m, _ = img.shape
-
-    # If a mask is not provided, create a default mask of all 1's
     if mask is None:
         mask = np.ones((n, m))
 
-    # Initialize an empty list to store the pixel values
-    casorati_matrix = []
+    casorati_matrix = [img[i2, i1, :] for i1, i2 in product(range(n), range(m)) if mask[i2, i1] != 0]
 
-    # Iterate through all pairs of indices (i1, i2) in the image
-    for i1, i2 in product(range(n), range(m)):
-        # If the mask value at this index is 0, skip this iteration
-        if mask[i2, i1] == 0:
-            continue
-
-        # Otherwise, append the pixel value at this index to the list
-        casorati_matrix.append(img[i2, i1, :])
-
-    # Convert the list of pixel values to a numpy array and return it
     return np.array(casorati_matrix)
 
 
-def cov(C_tilde):
-    # cov(C) = 1/ (n - 1) np.transpose(C_tilde) C_tilde = np.transpose(phi) ∧ phi
-    # where:    n       - number of z-spectra dynamics
-    #           C_tilde - colum-wise mean centered Casorati matrix
-    #           phi     - n x n orthogonal eigenvector matrix
-    #           ∧       - diag(λ_1, λ_2, ....) with λ_i being the eigenvalue i (λ_1 > λ_2 > .....)
-    cov = 1 / (C_tilde.shape[1] - 1) * np.dot(np.transpose(C_tilde), C_tilde)
-    return cov
+def cov(C_tilde: np.ndarray) -> np.ndarray:
+    """
+    Calculates the covariance matrix of the provided Casorati matrix.
+
+    Args:
+        C_tilde (np.ndarray): The column-wise mean centered Casorati matrix.
+
+    Returns:
+        np.ndarray: The covariance matrix of the provided Casorati matrix.
+    """
+    n = C_tilde.shape[1]
+    return 1 / (n - 1) * np.dot(np.transpose(C_tilde), C_tilde)
 
 
-def calc_eig(matrix: np.array, order: str) -> tuple[np.ndarray, np.ndarray]:
-    # Calculate the eigenvalues and eigenvectors of the matrix
+def calc_eig(matrix: np.ndarray, order: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculates and sorts the eigenvalues and eigenvectors of the provided matrix.
+
+    Args:
+        matrix (np.ndarray): The input matrix for which eigenvalues and eigenvectors are to be calculated.
+        order (str): A string indicating the order to sort the eigenvalues.
+                    'max' for descending and 'min' for ascending.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]:
+            - A sorted array of eigenvalues.
+            - A 2D array where each column is a sorted eigenvector.
+    """
     eigvals, eigvecs = np.linalg.eig(matrix)
 
-    # Determine which eigenvalues to sort based on the specified order
     if order == "max":
-        # Sort the eigenvalues in decreasing order
         idx = eigvals.argsort()[::-1]
-    if order == "min":
-        # Sort the eigenvalues in increasing order
+    elif order == "min":
         idx = eigvals.argsort()
+    else:
+        raise ValueError("Order must be either 'max' or 'min'.")
 
-    # Sort the eigenvalues and eigenvectors based on the indices obtained above
-    eigvals = eigvals[idx]
-    eigvecs = eigvecs[:, idx]
-
-    # Return the real parts of the sorted eigenvalues and eigenvectors
-    return np.real(eigvals), np.real(eigvecs)
+    return np.real(eigvals[idx]), np.real(eigvecs[:, idx])
