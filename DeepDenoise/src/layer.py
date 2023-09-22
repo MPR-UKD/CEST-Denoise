f3897cee769167c3eb52fa36c1876f01149b0a82
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-09-22 08:14:42 +0200

    Enhanced readability with comments and docstrings

--- DeepDenoise/src/layer.py
+++ DeepDenoise/src/layer.py
@@ -4,9 +4,16 @@ import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module):
-    """Double convolution block with BatchNorm and ReLU activation."""
+    """
+    A module for performing double convolution, consisting of two sequential convolutional layers
+    each followed by Batch Normalization and ReLU activation.
 
-    def __init__(self, in_channels: int, out_channels: int):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
@@ -18,64 +25,86 @@ class DoubleConv(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the double convolution block."""
         return self.double_conv(x)
 
 
 class Down(nn.Module):
-    """Downsampling block with MaxPool followed by a DoubleConv."""
+    """
+    A module for downsampling the feature map using Max Pooling followed by a double convolution block.
 
-    def __init__(self, in_channels: int, out_channels: int):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2), DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the downsampling block."""
         return self.maxpool_conv(x)
 
 
 class Up(nn.Module):
-    """Upsampling block with either bilinear upsampling or transposed convolution followed by a DoubleConv."""
+    """
+    A module for upsampling the feature map either using bilinear upsampling or transposed convolution,
+    followed by a double convolution block.
 
-    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        bilinear (bool): If True, uses bilinear upsampling, otherwise uses transposed convolution.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True) -> None:
         super().__init__()
-
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(
-                in_channels // 2, in_channels // 2, kernel_size=2, stride=2
-            )
-
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True) if bilinear else \
+            nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the upsampling block."""
         x1 = self.up(x1)
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
+        diffY, diffX = x2.size()[2] - x1.size()[2], x2.size()[3] - x1.size()[3]
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
 class OutConv(nn.Module):
-    """Output convolution block with a 1x1 kernel size."""
+    """
+    A module representing the output convolution with a 1x1 kernel size.
 
-    def __init__(self, in_channels: int, out_channels: int):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the output convolution block."""
         return self.conv(x)
 
 
 class ResDown(nn.Module):
-    """Residual layer block with two convolutional layers and a shortcut connection."""
+    """
+    A module representing a residual down block with two convolutional layers, Batch Normalization, ReLU,
+    and a shortcut connection.
 
-    def __init__(self, in_channels: int, out_channels: int):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
-
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu1 = nn.ReLU(inplace=True)
@@ -84,71 +113,52 @@ class ResDown(nn.Module):
         self.relu2 = nn.ReLU(inplace=True)
 
         # 1x1 convolutional layer to change the number of channels in the residual
-        self.shortcut = nn.Conv2d(
-            in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False
-        )
+        self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn_shortcut = nn.BatchNorm2d(out_channels)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the residual down block."""
         residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
+        out = self.relu1(self.bn1(self.conv1(x)))
+        out = self.relu2(self.bn2(self.conv2(out)))
 
         # Update residual to match the number of channels
-        residual = self.shortcut(residual)
-        residual = self.bn_shortcut(residual)
-
+        residual = self.bn_shortcut(self.shortcut(residual))
         out += residual
-        out = self.relu2(out)
-
         return out
 
 
 class ResUp(nn.Module):
-    """Residual upsampling block with either bilinear upsampling or transposed convolution followed by a DoubleConv and a residual connection."""
+    """
+    A module representing a residual up block with either bilinear upsampling or transposed convolution,
+    followed by a double convolution block and a residual connection.
 
-    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True):
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        bilinear (bool): If True, uses bilinear upsampling, otherwise uses transposed convolution.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, bilinear: bool = True) -> None:
         super().__init__()
-
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(
-                in_channels // 2, in_channels // 2, kernel_size=2, stride=2
-            )
-
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True) if bilinear else \
+            nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels, out_channels)
 
         # Residual connection
         self.shortcut = nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size=1,
-                stride=1,
-                padding=0,
-                bias=False,
-            ),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(out_channels),
         )
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the residual up block."""
         x1 = self.up(x1)
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
+        diffY, diffX = x2.size()[2] - x1.size()[2], x2.size()[3] - x1.size()[3]
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
 
         # Residual connection
         residual = self.shortcut(x)
-
-        out = self.conv(x)
-        out = out + residual
-
+        out = self.conv(x) + residual
         return out
